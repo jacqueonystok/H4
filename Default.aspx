4d1afa15916cd03f3f2bf888b338cb34ca086089
unknown <jacqueline-onystok@uiowa.edu>
2013-10-09 18:03:23 -0500

    added working calc

---
+++ Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+   <link href="StyleSheet.css" rel="stylesheet" type="text/css" />  <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+  <div id="intro"><h1> Welcome to Salary Calculator!</h1> </div>
+<br /><br />
+Hourly Wage: <asp:TextBox ID="TbHourlyWage" runat="server" ></asp:TextBox>
+<br /><br />
+Hours Work This Week: <asp:TextBox ID="tbHoursWorked" runat="server"
+></asp:TextBox>
+<br /><br />
+Pre-Tax Deductions: <asp:TextBox ID="tbPreTax" runat="server" ></asp:TextBox>
+    <br />
+    <br />
+    After Tax Deductions:
+    <asp:TextBox ID="TbAfterTax" runat="server"></asp:TextBox>
+<br /><br />
+   <div id="pay"><h2> Weekly Net Pay:</div> &nbsp; <asp:Label ID="lblNetWeekPay" runat="server"></asp:Label></h2></div>
+    <br />
+<br />
+<asp:Button ID="BtnCalcPmt" runat="server" Text="Calculate" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Button ID="BTClear" runat="server" Text="Clear" /><br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,69 @@
+﻿'Be sure to import system.data so you can create data tables on the fly.
+Imports System.Data
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+
+
+    Protected Sub btnCalcPmt_Click(sender As Object, e As EventArgs) Handles btnCalcPmt.Click
+        'Declaring the Variables for each field.
+        Dim hourlyWage As Double
+        Dim hoursWorked As Double
+        Dim preTax As Double
+        Dim afterTax As Double
+
+
+        'Declaring variables in equations
+        Dim netPay As Double
+        Dim pay As Double
+        Dim taxAMNT As Double
+        Dim taxable As Double
+        Dim posttax As Double
+
+
+        'This Section Converts each input string to the appropriate variable assigned.
+        hourlyWage = CDbl(tbHourlyWage.Text)
+        hoursWorked = CDbl(tbHoursWorked.Text)
+        preTax = CDbl(tbPreTax.Text)
+        afterTax = CDbl(TbAfterTax.Text)
+
+
+        'This Section Formats the Loan Input to Currency.
+        TbHourlyWage.Text = FormatCurrency(hourlyWage)
+        tbPreTax.Text = FormatCurrency(preTax)
+        TbAfterTax.Text = FormatCurrency(afterTax)
+
+    
+
+        'Calculating Pay
+        pay = (hourlyWage * hoursWorked)
+
+        'calc taxable
+        taxable = (pay - preTax)
+        'calcualting taxes
+        If taxable < 500 Then
+            taxAMNT = (0.18 * taxable)
+        Else
+            taxAMNT = (0.22 * taxable)
+        End If
+
+        'find post tax amount
+        posttax = (taxable - taxAMNT)
+
+        'finding net pay
+        netPay = (posttax - afterTax)
+
+        lblNetWeekPay.Text = FormatCurrency(netPay)
+
+    End Sub
+    'clearing after user clicks
+    Protected Sub BTClear_Click(sender As Object, e As EventArgs) Handles BTClear.Click
+        tbHourlyWage.Text = ""
+        tbPreTax.Text = ""
+        TbAfterTax.Text = ""
+        lblNetWeekPay.Text = ""
+        tbHoursWorked.Text = ""
+    End Sub
+
+End Class
+
